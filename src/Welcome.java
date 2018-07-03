7e30a4cfe1bf0418870c82e87d5febcfb72bc3c2
League Student <league@iMac-6.attlocal.net>
2018-07-03 16:35:07 -0700

    Welcome

--- src/Welcome.java
+++ src/Welcome.java
@@ -12,6 +12,6 @@ public class Welcome {
 		r2d2.setSpeed(10);
 		r2d2.move(400);
 		JOptionPane.showMessageDialog(null, "WELCOME TO LEVEL 0!!");
-		System.out.println("I love programming");
+		System.out.println("I really love programming");
 	}
 }
